310150fa3476ab512585e5c9bee27bbd6454c5c8
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2024-01-11 16:24:04 +0530

    variables define and declaration

---
+++ Week1 Fundamental/variables2.java
@@ -0,0 +1,16 @@
+public class variables2 {
+    public static void main(String args[])
+    {
+        //variables and decalration
+        int num1=4, num2=5;
+        int result = num1+num2;
+
+        //evaluatating the expression
+        System.out.println(3*9);
+        System.out.println(3+9);
+
+        //Printing the variable values
+        System.out.println(result);
+
+    }    
+}
