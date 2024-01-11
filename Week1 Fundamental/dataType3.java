aeffbe87cf92e5ccb85d3e59f55c97b76f58c811
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2024-01-11 16:41:13 +0530

    datatypes

---
+++ Week1 Fundamental/dataType3.java
@@ -0,0 +1,18 @@
+public class dataType3 {
+    public static void main(String args[])
+    {
+        // primitive datatypes 
+        //Integer
+        int num1 =123;
+        byte by=127;
+        short sh=345;
+        long l =678l;
+        //float
+        float f=7.6f;
+        double d= 5.6;
+        //character
+        char c='k';
+        //boolean
+        boolean b= true;
+    }
+}
