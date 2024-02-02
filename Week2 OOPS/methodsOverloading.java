eda7934b3b934357d11bc8d5736888447b7ba7c2
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2024-02-02 18:46:20 +0530

    method overloading

---
+++ Week2 OOPS/methodsOverloading.java
@@ -0,0 +1,26 @@
+class Calculator
+{
+    public int add(int n1 , int n2)
+    {
+        return n1+n2;
+    }
+
+    public int add(int n1 , int n2 , int n3)
+    {
+        return n1+ n2+ n3;
+    }
+}
+
+
+public class methodsOverloading {
+    public static void main(String args[])
+    {
+        Calculator obj = new Calculator();
+        int r1 = obj.add(3,4);
+        int r2 = obj.add(3,4,5);
+
+        System.out.println("r1 is="+ r1 + " r2 is ="+ r2);
+
+
+    }
+}
